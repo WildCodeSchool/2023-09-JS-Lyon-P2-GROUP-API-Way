643aae90e857b57de9f88585005ed692900f9f10
techmefr <compigni.gaetan@gmail.com>
2023-10-26 15:16:51 +0200

    change navbar and add searchbutton i will write the funtion after css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Navbar from "./NavBar";
+import Navbar from "./components/NavBar/NavBar";
 import Cards from "./components/Cards/Cards";
 import DateList from "./components/DateList/DateList";
 
--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
+import SearchButton from "./SearchButton"; // Importez le composant SearchButton
 
 function Navbar({ handleSearchClick }) {
   return (
     <div className="navbar">
-      <img src="/src/assets/logo2.png" alt="Logo Dhéliat Chronicle" />
-      <h1>Dhéliat Chronicle</h1>
-      <button type="button" onClick={handleSearchClick}>
-        Recherche
-      </button>
+      <SearchButton onClick={handleSearchClick} />{" "}
+      {/* Utilisez le composant SearchButton à gauche */}
+      <div className="center-content">
+        <img src="/src/assets/logo2.png" alt="Logo Dhéliat Chronicle" />
+        <h1>Dhéliat Chronicle</h1>
+      </div>
+      <SearchButton onClick={handleSearchClick} />{" "}
+      {/* Utilisez le composant SearchButton à droite */}
     </div>
   );
 }
