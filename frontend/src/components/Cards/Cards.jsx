c747327fd8a86c8958fb86384371e538da2ad93f
celineblle <celineblle@outlook.fr>
2023-11-07 17:15:36 +0100

    ajout de presque toute la user

--- frontend/src/components/Cards/Cards.jsx
+++ frontend/src/components/Cards/Cards.jsx
@@ -2,36 +2,94 @@ import Colors from "./Colors";
 import ApiCall from "./ApiCall";
 import Database from "./DataBase/Database";
 import "./card.css";
+import cloudSun from "./assets/cloud-sun.svg";
+import cloud from "./assets/cloud.svg";
+import rain from "./assets/rain.svg";
+import smog from "./assets/smog.svg";
+import snowflake from "./assets/snowflake.svg";
+import storm from "./assets/storm.svg";
+import sunRain from "./assets/sun-rain.svg";
+import sun from "./assets/sun.svg";
+
+const meteoVisuel = [
+  {
+    name: "clear sky",
+    class: "clearSky card",
+    image: sun,
+    description: "soleil",
+  },
+  {
+    name: "few clouds",
+    class: "fewClouds card",
+    image: cloudSun,
+    description: "partielement nuageux",
+  },
+  {
+    name: "scattered clouds",
+    class: "scatteredClouds card",
+    image: cloud,
+    description: "nuageux",
+  },
+  {
+    name: "broken clouds",
+    class: "brokenClouds card",
+    image: cloud,
+    description: "nuages gris",
+  },
+  {
+    name: "shower rain",
+    class: "showerRain card",
+    image: rain,
+    description: "averses",
+  },
+  {
+    name: "rain",
+    class: "rain card",
+    image: sunRain,
+    description: "pluie",
+  },
+  {
+    name: "thunderstorm",
+    class: "thunderstorm card",
+    image: storm,
+    description: "orageux",
+  },
+  {
+    name: "snow",
+    class: "snow card",
+    image: snowflake,
+    description: "neige",
+  },
+  {
+    name: "mist",
+    class: "mist card",
+    image: smog,
+    description: "brouillard",
+  },
+];
 
 function Cards() {
   return (
     <div>
-      <div className="card cardTop">
-        <h3>58/42</h3>
+      <div className={meteoVisuel[0].class}>
+        <h3 className="date">58/42</h3>
         <div className="cardContent">
-          <svg fill="white" viewBox="0 0 512 512" width="87px" height="87px">
-            <path d="M361.5 1.2c5 2.1 8.6 6.6 9.6 11.9L391 121l107.9 19.8c5.3 1 9.8 4.6 11.9 9.6s1.5 10.7-1.6 15.2L446.9 256l62.3 90.3c3.1 4.5 3.7 10.2 1.6 15.2s-6.6 8.6-11.9 9.6L391 391 371.1 498.9c-1 5.3-4.6 9.8-9.6 11.9s-10.7 1.5-15.2-1.6L256 446.9l-90.3 62.3c-4.5 3.1-10.2 3.7-15.2 1.6s-8.6-6.6-9.6-11.9L121 391 13.1 371.1c-5.3-1-9.8-4.6-11.9-9.6s-1.5-10.7 1.6-15.2L65.1 256 2.8 165.7c-3.1-4.5-3.7-10.2-1.6-15.2s6.6-8.6 11.9-9.6L121 121 140.9 13.1c1-5.3 4.6-9.8 9.6-11.9s10.7-1.5 15.2 1.6L256 65.1 346.3 2.8c4.5-3.1 10.2-3.7 15.2-1.6zM160 256a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zm224 0a128 128 0 1 0 -256 0 128 128 0 1 0 256 0z" />
-          </svg>
-          <p>37°</p>
+          <img src={meteoVisuel[0].image} alt={meteoVisuel[0].description} />
+          <p className="temperature">37°</p>
         </div>
       </div>
       <div className="card cardMiddle">
-        <h3>59/42</h3>
+        <h3 className="date">59/42</h3>
         <div className="cardContent">
-          <svg fill="white" viewBox="0 0 640 512" width="87px" height="87px">
-            <path d="M294.2 1.2c5.1 2.1 8.7 6.7 9.6 12.1l14.1 84.7 84.7 14.1c5.4 .9 10 4.5 12.1 9.6s1.5 10.9-1.6 15.4l-38.5 55c-2.2-.1-4.4-.2-6.7-.2c-23.3 0-45.1 6.2-64 17.1l0-1.1c0-53-43-96-96-96s-96 43-96 96s43 96 96 96c8.1 0 15.9-1 23.4-2.9c-36.6 18.1-63.3 53.1-69.8 94.9l-24.4 17c-4.5 3.2-10.3 3.8-15.4 1.6s-8.7-6.7-9.6-12.1L98.1 317.9 13.4 303.8c-5.4-.9-10-4.5-12.1-9.6s-1.5-10.9 1.6-15.4L52.5 208 2.9 137.2c-3.2-4.5-3.8-10.3-1.6-15.4s6.7-8.7 12.1-9.6L98.1 98.1l14.1-84.7c.9-5.4 4.5-10 9.6-12.1s10.9-1.5 15.4 1.6L208 52.5 278.8 2.9c4.5-3.2 10.3-3.8 15.4-1.6zM144 208a64 64 0 1 1 128 0 64 64 0 1 1 -128 0zM639.9 431.9c0 44.2-35.8 80-80 80H288c-53 0-96-43-96-96c0-47.6 34.6-87 80-94.6l0-1.3c0-53 43-96 96-96c34.9 0 65.4 18.6 82.2 46.4c13-9.1 28.8-14.4 45.8-14.4c44.2 0 80 35.8 80 80c0 5.9-.6 11.7-1.9 17.2c37.4 6.7 65.8 39.4 65.8 78.7z" />
-          </svg>
-          <p>18°</p>
+          <img src={meteoVisuel[1].image} alt={meteoVisuel[1].description} />
+          <p className="temperature">18°</p>
         </div>
       </div>
       <div className="card cardBottom">
-        <h3>60/42</h3>
+        <h3 className="date">60/42</h3>
         <div className="cardContent">
-          <svg fill="white" viewBox="0 0 512 512" width="87px" height="87px">
-            {" "}
-            <path d="M96 320c-53 0-96-43-96-96c0-42.5 27.6-78.6 65.9-91.2C64.7 126.1 64 119.1 64 112C64 50.1 114.1 0 176 0c43.1 0 80.5 24.3 99.2 60c14.7-17.1 36.5-28 60.8-28c44.2 0 80 35.8 80 80c0 5.5-.6 10.8-1.6 16c.5 0 1.1 0 1.6 0c53 0 96 43 96 96s-43 96-96 96H96zM81.5 353.9c12.2 5.2 17.8 19.3 12.6 31.5l-48 112c-5.2 12.2-19.3 17.8-31.5 12.6S-3.3 490.7 1.9 478.5l48-112c5.2-12.2 19.3-17.8 31.5-12.6zm120 0c12.2 5.2 17.8 19.3 12.6 31.5l-48 112c-5.2 12.2-19.3 17.8-31.5 12.6s-17.8-19.3-12.6-31.5l48-112c5.2-12.2 19.3-17.8 31.5-12.6zm244.6 31.5l-48 112c-5.2 12.2-19.3 17.8-31.5 12.6s-17.8-19.3-12.6-31.5l48-112c5.2-12.2 19.3-17.8 31.5-12.6s17.8 19.3 12.6 31.5zM313.5 353.9c12.2 5.2 17.8 19.3 12.6 31.5l-48 112c-5.2 12.2-19.3 17.8-31.5 12.6s-17.8-19.3-12.6-31.5l48-112c5.2-12.2 19.3-17.8 31.5-12.6z" />{" "}
-          </svg>
-          <p>52°</p>
+          <img src={meteoVisuel[2].image} alt={meteoVisuel[2].description} />
+          <p className="temperature">52°</p>
         </div>
       </div>
       <Colors />
